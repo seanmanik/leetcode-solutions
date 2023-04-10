33d854915567bc8be853d103ed13f08a639ceb94
Sean <seanmanik@gmail.com>
2023-04-10 15:16:01 +0800

    ReverseNodesInKGroup solution update

--- Mediums/ReverseNodesInKGroup.java
+++ Mediums/ReverseNodesInKGroup.java
@@ -10,6 +10,38 @@
  */
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
-        
+        ListNode newTail = head;
+        ListNode nextHead = head;
+        if (head == null) {
+            return null;
+        }
+        int temp = k;
+        while (k > 1) {
+            if (nextHead.next == null) {
+                return head;
+            }
+            nextHead = nextHead.next;
+            k--;
+        }
+        ListNode newHead = nextHead;
+
+        nextHead = nextHead.next;
+        newHead.next = null;
+        reverse(head);
+        newTail.next = reverseKGroup(nextHead, temp);
+        return newHead;
+    }
+
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr.next != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        curr.next = prev;
+        return curr;
     }
 }
